7f7347465186cdc1dea359287c5b8d339a7d1b26
MaxDrg <maximadda2001@gmail.com>
2023-05-25 07:09:26 +0200

    optimize

--- webparser/options/admin.py
+++ webparser/options/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Region, Speciality, Education, Qualification, Education_base
 
 admin.site.register(Education)
-admin.site.register(Qualification)
-admin.site.register(Education_base)
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'registry_id']
+
+admin.site.register(Qualification, EducationAdmin)
+admin.site.register(Education_base, EducationAdmin)
+
 admin.site.register(Region)
 admin.site.register(Speciality)
